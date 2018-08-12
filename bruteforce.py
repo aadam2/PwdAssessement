165af23e0f2e3368c09c688ae677faf775e237be
aadam2 <aadam.git@gmail.com>
2018-08-12 03:13:45 +0200

    Building charset

--- bruteforce.py
+++ bruteforce.py
@@ -2,6 +2,7 @@
 
 import string
 import optparse
+import operator
 
 hashing_algos = {32:"MD5", 40:"SHA1", 64:"SHA256"}
 
@@ -11,7 +12,7 @@ def get_arguments():
     parser.add_option("-p", "--pwd-hash", dest="pwd_hash", help="Hash of the password to bruteforce")
     #parser.add_option("-l", "--lowercase", action="store_true", dest="lowercase", help="Plain password may contain lowercase characters")
     parser.add_option("-u", "--uppercase", action="store_true", dest="uppercase", help="Plain password may contain uppercase characters")
-    parser.add_option("-n", "--numbers", action="store_true", dest="numbers", help="Plain password may contain numbers")
+    parser.add_option("-d", "--digits", action="store_true", dest="digits", help="Plain password may contain digits")
     parser.add_option("-s", "--special", action="store_true", dest="special", help="Plain password may contain special characters")
     parser.add_option("--length", dest="length", help="Plain password's length")
     parser.add_option("-a", "--all", action="store_true", dest="all", help="Use this option if you have no prior knowledge on the plain password")
@@ -23,16 +24,30 @@ def get_arguments():
 
     return options
 
+def flatten(l):
+    flat_list = [item for sublist in l for item in sublist]
+    return flat_list
 
 def build_charset(options):
+    if options.all:
+        charset = list(string.ascii_letters)
+        charset.append(list(string.digits))
+        charset.append(list(string.punctuation))
+        return flatten(charset)
     charset = list(string.ascii_lowercase)
-    print("Current charset : {0}".format(charset))
+    if options.uppercase:
+        charset.append(list(string.ascii_uppercase))
+    if options.digits:
+        charset.append(list(string.digits))
+    if options.special:
+        charset.append(list(string.punctuation))
+    return flatten(charset)
 
 def main():
 
     options = get_arguments()
     charset = build_charset(options)
-
+    print("charset : {0}".format(charset))
 
 if __name__ == "__main__":
     main()
