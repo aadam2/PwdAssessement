a253f19184712377ad4af9c375f75ed32a14ff16
aadam2 <aadam.git@gmail.com>
2018-08-19 22:05:03 +0200

    Recursive approach

--- bruteforce.py
+++ bruteforce.py
@@ -3,6 +3,7 @@
 import string
 import optparse
 import hashlib
+import sys
 
 hashing_algos = {32:"MD5", 40:"SHA1", 64:"SHA256"}
 
@@ -26,46 +27,73 @@ def get_arguments():
     return options
 
 
-def flatten(l):
-    flat_list = [item for sublist in l for item in sublist]
-    return flat_list
-
-
 def build_charset(options):
     if options.all:
-        charset = list(string.ascii_letters)
-        charset.append(list(string.digits))
-        charset.append(list(string.punctuation))
-        return flatten(charset)
-    charset = list(string.ascii_lowercase)
+        charset = string.ascii_letters
+        charset+=string.digits
+        charset+=string.punctuation
+        return charset
+    charset = string.ascii_lowercase
     if options.uppercase:
-        charset.append(list(string.ascii_uppercase))
+        charset+=string.ascii_uppercase
     if options.digits:
-        charset.append(list(string.digits))
+        charset+=string.digits
     if options.special:
-        charset.append(list(string.punctuation))
-    return flatten(charset)
+        charset+=string.punctuation
+    return charset
 
 
-def bruteforce(charset, options):
-    if len(options.pwd-hash) == 32:
+def similar_hash(attempt, pwd_hash):
+    if len(pwd_hash) == 32:
         m = hashlib.md5
-    elif len(options.pwd-hash) == 40:
+    elif len(pwd_hash) == 40:
         m = hashlib.sha1
-    elif len(options.pwd-hash) == 64:
+    elif len(pwd_hash) == 64:
         m = hashlib.sha256
 
-    pwd_length = 2
-    if options.length:
-        pwd_length = int(options.length)
+    # if attempt == "dddd":
+    #     print("attempt : {0}".format(attempt))
+    #     print("m(attempt).hexdigest() : {0}".format(m(str(attempt)).hexdigest()))
+    #     print("pwd_hash : {0}".format(pwd_hash))
+    #     raw_input("OK")
 
-    
+    if m(attempt).hexdigest() == pwd_hash:
+        return True
+    else:
+        return False
+
+
+def bruteforce(attempt, position, size, pwd_hash, charset):
+    # print(" ** size : {0}".format(size))
+    # print(" ** position : {0}".format(position))
+    # print(" ** charset : {0}".format(charset))
+    # print(" ** charset[0] : {0}".format(charset[0]))
+    new_attempt = attempt
+    if position < size:
+        for i in range(len(charset)):
+            new_attempt = attempt
+            new_attempt += charset[i]
+            bruteforce(new_attempt, position+1, size, pwd_hash, charset)
+    else:
+        print("[+] Testing with : {0}".format(new_attempt))
+        if similar_hash(new_attempt, pwd_hash):
+            #print("Found : {0}".format(new_attempt))
+            sys.exit("Found : {0}".format(new_attempt))
 
 
 def main():
     options = get_arguments()
     charset = build_charset(options)
-    print("charset : {0}".format(charset))
+    print("Charset : {0}".format(charset))
+
+    print("options.length : {0}".format(options.length))
+    print("options.pwd_hash : {0}".format(options.pwd_hash))
+
+    if options.length:
+        bruteforce("", 0, int(options.length), options.pwd_hash, charset)
+    else:
+        for i in range(1, 10):
+            bruteforce("", 0, i, options.pwd_hash, charset)
 
 
 if __name__ == "__main__":
